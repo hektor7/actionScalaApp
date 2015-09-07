8c13ce22113eeeed25528eac9dbe72b968666ef6
Héctor Vizcaíno <hektor7@gmail.com>
2015-09-07 21:35:39 +0200

    Actions to get artists by name, get artists by name and country and get artists by contry and optionally by name at same time (name optional, contry mandatory). Routes for them

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,5 +12,34 @@ object Application extends Controller {
   def listArtist = Action {
 	Ok(views.html.home(Artist.fetch))
   }
+  
+  def fetchArtistByName(name:String) = Action { 
+     Ok(views.html.home(Artist.fetchByName(name))) 
+  }
+  
+  def search(name: String, country: String) = Action { 
+    val result = Artist.fetchByNameOrCountry(name, country) 
+    if(result.isEmpty){
+      NoContent
+       } 
+    else {
+      Ok(views.html.home(result))
+    }
+    
+    
+  }
+  
+  def search2(name: Option[String], country: String) = Action { 
+    val result = name match{ 
+      case Some(n) => Artist.fetchByNameOrCountry(n, country) 
+      case None => Artist.fetchByCountry(country) 
+    } 
+    if(result.isEmpty){
+      NoContent
+    } 
+    else { 
+      Ok(views.html.home(result)) 
+    } 
+  }
 
 }
